a9e5775d8b461387b8ba64c7ac851585df4de078
JeelDobariya38 <jeeldobariya38@gmail.com>
2024-07-01 21:49:21 +0530

    extract the script content from webpage
    TODO: code in file compile.js need optimation in terms of readability

---
+++ scripts/compile.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#copybtn").addEventListener("click", () => {
+        let scriptcontent = document.querySelector(".script-content");
+        let childrens = scriptcontent.childNodes;
+        let scriptTokens = [];
+
+        childrens.forEach((elem, indx) => {
+            let token = {
+                id: indx,
+            };
+
+            if (elem.id == "printmsg") {
+                token.id = "printmsg";
+                elem.childNodes.forEach((childElem) => {
+                    if (childElem.id == "details") {
+                        childElem.childNodes.forEach((detailChildElem) => {
+                            if (detailChildElem.id == "messageinput") {
+                                token.message = detailChildElem.value;
+                            }
+                        });
+                    }
+                });
+            }
+
+            else if (elem.id == "mkdir") {
+                token.id = "mkdir";
+                elem.childNodes.forEach((childElem) => {
+                    if (childElem.id == "details") {
+                        childElem.childNodes.forEach((detailChildElem) => {
+                            if (detailChildElem.id == "directorynameinput") {
+                                token.dirname = detailChildElem.value;
+                            }
+                        });
+                    }
+                });
+            }
+
+            else if (elem.id == "mkfile") {
+                token.id = "mkfile";
+                elem.childNodes.forEach((childElem) => {
+                    if (childElem.id == "details") {
+                        childElem.childNodes.forEach((detailChildElem) => {
+                            if (detailChildElem.id == "filenameinput") {
+                                token.filename = detailChildElem.value;
+                            }
+                        });
+                    }
+                });
+            }
+
+            else if (elem.id == "writefile") {
+                token.id = "writefile";
+                elem.childNodes.forEach((childElem) => {
+                    if (childElem.id == "details") {
+                        childElem.childNodes.forEach((detailChildElem) => {
+                            if (detailChildElem.id == "filenameinput") {
+                                token.filename = detailChildElem.value;
+                            }
+                            if (detailChildElem.id == "contentinput") {
+                                token.content = detailChildElem.value;
+                            }
+                        });
+                    }
+                });
+            }
+
+            else {
+                return;
+            }
+
+            scriptTokens.push(token);
+        });
+
+        console.log(scriptTokens);
+    });
+});
