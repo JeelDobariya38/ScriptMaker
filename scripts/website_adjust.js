b63832516811041b9771ea9a401498fbfd2a3016
JeelDobariya38 <jeeldobariya38@gmail.com>
2024-07-02 22:09:35 +0530

    Create a script that change page based on place it opened

---
+++ scripts/website_adjust.js
@@ -0,0 +1,24 @@
+function getMobileWebsite() {
+    return window.location.href.replace("index.html", "smallscreens_homepage.html");
+}
+
+function getDestopWebsite() {
+    return window.location.href.replace("smallscreens_homepage.html", "index.html");
+}
+
+
+function checkScreenWidth() {
+    if (window.innerWidth <= 650) {
+        console.log("It seems like you are on a Phone!!!!");
+        if (window.location.href.includes("index.html")) {
+            window.location.href = getMobileWebsite();
+        }
+    } else {
+        console.log("It seems like you are on a Desktop!!!!");
+        if (window.location.href.includes("homepage.html")) {
+            window.location.href = getDestopWebsite();
+        }
+    }
+}
+
+window.onload = checkScreenWidth;
